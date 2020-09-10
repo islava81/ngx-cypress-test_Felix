78629f254e5462fe84dd4272f73d2b48f63ea5ef
islava81 <antonio_islava@hotmail.com>
2020-09-09 19:18:58 -0700

    Visual Testing spec file and snapshots

---
+++ cypress/visualTest.spec.js
@@ -0,0 +1,21 @@
+
+describe('visual testing', () => {
+
+    it('should test snapshot', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+
+        cy.contains('nb-card','Using the Grid').then( firstForm => {
+            
+            cy.wrap(firstForm).toMatchImageSnapshot()
+            cy.document().toMatchImageSnapshot()
+                      
+
+        })
+
+    })
+
+})
